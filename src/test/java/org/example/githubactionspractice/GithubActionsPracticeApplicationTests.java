b515d8c77d53c395f83a2365186fead48a5b9a3b
Eongyu Park <peg5325@gmail.com>
2025-06-26 17:50:23 +0900

    First Commit: Github actions 와 관련된 파이도 같이 commit

---
+++ src/test/java/org/example/githubactionspractice/GithubActionsPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.githubactionspractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
